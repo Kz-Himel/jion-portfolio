ee2121885a3a1d10f64b5d2b5a72d32782464eca
Himel <kzhimel129@gmail.com>
2026-06-05 17:12:14 +0600

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
   reactCompiler: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
