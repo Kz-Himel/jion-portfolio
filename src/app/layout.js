4f13b28f2db2dbc14637cdc18840967273cf0e3e
Kz-Himel <kzhimel129@gmail.com>
2026-08-05 09:40:52 +0600

    Added: Icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,9 @@ import "./globals.css";
 
 export const metadata = {
   title: "Md Jion Mia | Graphics Designer Portfolio",
-
   description:
     "Premium Graphics Designer & Brand Identity Expert",
+    icons: "/images/logo.png"
 };
 
 export default function RootLayout({ children }) {
